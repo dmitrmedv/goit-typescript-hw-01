86164ac34cb7b317cd5032f168005668feab6167
dmitrmedv <dmitrmedv@gmail.com>
2024-12-19 11:49:10 +0200

    add

--- src/generics/1.ts
+++ src/generics/1.ts
@@ -1,10 +1,14 @@
 import axios from "axios";
 
-// async function fetchData(url: string): Promise<string> {
-//   try {
-//     const response = await axios.get(url);
-//     return response.data;
-//   } catch (error) {
-//     throw new Error(`Error fetching from ${url}: ${error}`);
-//   }
-// }
+async function fetchData<T>(url: string): Promise<T> {
+  try {
+    const response = await axios.get<T>(url);
+    return response.data;
+  } catch (error) {
+    throw new Error(`Error fetching from ${url}: ${error}`);
+  }
+}
+
+function fnAdd<T>(arr: T[]): T {
+  return arr[0];
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,1 @@
 import "./style.css";
-
-type Person = {
-  name: string;
-  age: number;
-  location: string;
-};
-type PersonKeys = keyof Person; // 'name' | 'age' | 'location'
