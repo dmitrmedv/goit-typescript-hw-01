2e9d2c9b08bfe496c185d2cb132cb12fe73fdb48
dmitrmedv <dmitrmedv@gmail.com>
2024-12-21 20:12:48 +0200

    add HM

--- src/generics/1.ts
+++ src/generics/1.ts
@@ -1,14 +1,10 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 
 async function fetchData<T>(url: string): Promise<T> {
   try {
-    const response = await axios.get<T>(url);
+    const response: AxiosResponse<T> = await axios.get(url);
     return response.data;
   } catch (error) {
     throw new Error(`Error fetching from ${url}: ${error}`);
   }
-}
-
-function fnAdd<T>(arr: T[]): T {
-  return arr[0];
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,1 @@
 import "./style.css";
-
-enum Names {
-  user1 = "user1",
-  user2 = "user2",
-  user3 = "user3",
-}
-
-let user: Names = Names.user1;
-console.log(user);
